9209ecc7651729e52eae134b06f470a2b4d4b846
zwj4386 <31980852+zwj4386@users.noreply.github.com>
2017-12-06 09:10:54 +0800

    Add files via upload

---
+++ my_blog/article/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+from django.http import Http404
+# Create your views here.
+
+def detail(request,id):
+	try:
+		post = Article.objects.get(id=str(id))
+	except Article.DoesNotExist:
+		raise Http404
+
+	return render(request, 'post.html', {'post':post})
+
+def home(request):
+	post_list = Article.objects.all()
+	return render(request, 'home.html', {'post_list':post_list})
+
+def test(request):
+	return render(request, 'test.html', {'current_time': datetime.now()})
